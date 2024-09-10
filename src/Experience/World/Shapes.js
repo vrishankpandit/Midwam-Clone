c6a6c7f1a3f5a1d9b0d89541ea2e81a845f24610
Vishu <vrishankpandit1344@gmail.com>
2024-09-10 16:27:17 +0530

    feat:Shapes Debug Menu

--- src/Experience/World/Shapes.js
+++ src/Experience/World/Shapes.js
@@ -18,10 +18,54 @@ export default class Shapes{
 
     addModel(){
         this.model=this.instance.scene;
-        this.model.scale.set(0.5,0.5,0.5)
-        this.model.position.set(10,-9,4)
+        this.model.scale.set(0.55,0.55,0.55)
+        this.model.rotation.x=-Math.PI/10;
+        this.model.position.set(9,-8,4)
         this.scene.add(this.model)
 
+        if(this.debug.active){
+            this.debug.ui
+            .add(this.model.position,'x')
+            .name('PositionX')
+            .min(0)
+            .max(20)
+            .step(0.001)
+            
+            this.debug.ui
+            .add(this.model.position,'y')
+            .name('PositionY')
+            .min(0)
+            .max(20)
+            .step(0.001)
+
+            this.debug.ui
+            .add(this.model.position,'z')
+            .name('PositionZ')
+            .min(0)
+            .max(20)
+            .step(0.001)
+
+            this.debug.ui
+            .add(this.model.rotation,'x')
+            .name('RotationX')
+            .min(0)
+            .max(Math.PI)
+            .step(0.001)
+            
+            this.debug.ui
+            .add(this.model.position,'y')
+            .name('RotationY')
+            .min(0)
+            .max(Math.PI)
+            .step(0.001)
+
+            this.debug.ui
+            .add(this.model.position,'z')
+            .name('RotationZ')
+            .min(0)
+            .max(Math.PI)
+            .step(0.001)
+        }
     }
 
 
